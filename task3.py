879e4ae970b0ef13c5359e71cb43f0888544be90
elvin1990huseynlii <161587697+elvin1990huseynlii@users.noreply.github.com>
2024-03-14 00:35:13 +0400

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,52 @@
+# Tapşırıq 1: 
+
+# İstifadəçidən boyu ilə bağlı məlumat alan və ona tövsiyə olunan ideal cəki aralığını göstərən proqram hazırlayın. 
+ 
+    
+# Əhatə olunan biliklər: İnput, Variables, Float, If...Else.
+
+
+# height = int(input('Boyunuzu daxil edin (santimetr olaraq): '))
+
+
+# if height < 150:
+#     print('Sizin çəkiniz 50-60 kq arasında olmalıdı.' )
+# elif height > 150 and height < 170:
+#     print('Sizin çəkiniz 60-70 kq arasında olmalıdı.' )
+# elif height > 170 and height < 190:
+#     print('Sizin çəkiniz 70-90 kq arasında olmalıdı.' )
+# elif height > 200 and height < 220:
+#     print('Sizin çəkiniz 90-110 kq arasında olmalıdı.' )
+# else:
+#     print('Sizin çəkiniz 120-140 kq arasında olmalıdı.' )
+
+
+
+
+
+
+
+# Tapşırıq 3: 
+
+# Müştəriyə səyahətin ümumi dəyərini hesablamaq üçün nəqliyyat şirkətinə Python proqramı yazın. 
+# İstifadəçidən səyahət məsafəsini və nəqliyyat vasitəsinin növünü (məsələn, avtomobil, yük maşını, avtobus) 
+# daxil etməyi təklif edin. Verilən məsafəyə və nəqliyyat vasitəsinin növünə əsaslanaraq ümumi dəyəri hesablayın 
+# (avtomobillərin hər km üçün 0,50 dollar, yük maşınlarının hər km üçün 1,00 dollar, 
+#  avtobusların hər km üçün qiyməti 2,00 dollar).
+
+
+# Əhatə olunan biliklər: İnput, Variables, Float, Əsas Operatorlar, If...Else.
+
+
+# vehicules = int(input("(1) bus, (2) car, (3) truck:  "))
+# distance = int(input("Please enter distance "))
+# if vehicules==1:
+#     print(distance*2)
+# elif vehicules==2:
+#     print(distance*0.5)
+# else:
+#     print(distance)
+      
+
+
+
